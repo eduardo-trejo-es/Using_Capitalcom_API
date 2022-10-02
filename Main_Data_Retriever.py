6b813cb9c2bce5225c3fcc9f0e400484dcb8f763
Eduardo <trejo.alfredo.es@outlook.com>
2022-10-02 12:35:27 -0500

    Geneted hour data frame

--- Main_Data_Retriever.py
+++ Main_Data_Retriever.py
@@ -9,10 +9,10 @@ from Class_Retriving_Data_and_Saving import CapitalAPI_Retriver_Piceses_Data
 
 
 
-Capital_Retriver = CapitalAPI_Retriver_Piceses_Data ('LPvUXaLcvh8fuQ0N')
+Capital_Retriver = CapitalAPI_Retriver_Piceses_Data ('iVxVv4E6D2Xl6YH4')
 #2022-09-30
 
-dates_From, DatesTo=Capital_Retriver.generateBussinesDaysrange("2018-03-26","2018-03-26")
+dates_From, DatesTo=Capital_Retriver.generateBussinesDaysrange("2018-03-26","2022-10-02")
 #Capital_Retriver.SavingDataPrices("2020-02-25T03:00:00", "2020-02-25T10:00:00","Twttr/DataMinuteTwttr.csv")
 for i in range(0,len(dates_From)):
     date_from=str(dates_From[i])
